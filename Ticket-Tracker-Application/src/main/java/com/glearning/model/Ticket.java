2e19d2ff7a8cc31d750373808c518d4606c80aa9
Aditya goswami <giriaditya26@gmail.com>
2023-07-13 11:15:35 +0530

    Group Assignment added

---
+++ Ticket-Tracker-Application/src/main/java/com/glearning/model/Ticket.java
@@ -0,0 +1,17 @@
+package com.glearning.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    // Constructors, getters, and setters
+}
--- src/main/java/com/glearning/emp/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.glearning.emp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.glearning.emp.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
